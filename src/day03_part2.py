9ee82e58d94fc951b32642a2d5c58f8d5f29a4b7
Daniel Pettersson <daniel.g.pettersson@gmail.com>
2021-12-03 21:27:58 +0100

    some cleanup

--- src/day03_part2.py
+++ src/day03_part2.py
@@ -1,6 +1,5 @@
 from collections import Counter
-from dataclasses import dataclass
-from typing import TextIO, Any, Iterable
+from typing import TextIO, Any, Iterable, List, Optional
 
 from src.common.base_day import BaseDay
 
@@ -9,14 +8,17 @@ class Day03(BaseDay):
 
     def execute(self, input_file: TextIO) -> Any:
         rows = [list(line) for line in input_file]
-        oxygen_ratings = self.get_val(rows, col=0, tie_value='1', n=0)
-        co2_scrubber_ratings = self.get_val(rows, col=0, tie_value='0', n=1)
+        oxygen_ratings = self.get_val(rows, tie_value='1', n=0)
+        co2_scrubber_ratings = self.get_val(rows, tie_value='0', n=1)
 
         oxygen_rating = int("".join(oxygen_ratings), base=2)
         co2_scrubber_rating = int("".join(co2_scrubber_ratings), base=2)
         return oxygen_rating * co2_scrubber_rating
 
-    def get_val(self, rows, col, tie_value, n):
+    def get_val(self, rows: List[List[str]], tie_value: str, n: int, col: Optional[int] = None):
+
+        if not col:
+            col = 0
 
         if len(rows) == 1:
             return rows[0]
@@ -25,10 +27,11 @@ class Day03(BaseDay):
         common = self.nth_common(cols[col], n, tie_value)
 
         new_rows = [r for r in rows if r[col] == common]
-        return self.get_val(new_rows, col + 1, tie_value, n)
+        return self.get_val(new_rows, tie_value, n, col=col + 1)
 
     @staticmethod
-    def nth_common(items, n, tie_value):
+    def nth_common(items: Iterable[str], n: int, tie_value: str):
+        assert 0 <= n <= 1
         most_common = Counter(items).most_common(2)
         if most_common[0][1] == most_common[1][1]:
             return tie_value
