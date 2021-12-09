76e7b222ac4bf257019d812eff2e204e4baa5833
Daniel Pettersson <daniel.g.pettersson@gmail.com>
2021-12-09 16:45:45 +0100

    Day 9 WIP

---
+++ src/day09_part1.py
@@ -0,0 +1,34 @@
+from collections import deque
+from typing import TextIO, Any, Optional
+
+from src.common.base_day import BaseDay
+
+
+class Day09(BaseDay):
+
+    def execute(self, input_file: TextIO) -> Any:
+        height_map = [[int(v) for v in line.strip()] for line in input_file]
+
+        width = len(height_map[0])
+        height = len(height_map)
+
+        risk_sum = 0
+
+        for y in range(0, height):
+            for x in range(0, width):
+                larger_neighbors = 0
+                for dx, dy in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
+                    xx = x + dx
+                    yy = y + dy
+                    if 0 <= xx < width and 0 <= yy < height:
+                        larger_neighbors += 1 if height_map[y][x] < height_map[yy][xx] else 0
+                    else:
+                        larger_neighbors += 1
+                if larger_neighbors == 4:
+                    risk_sum += height_map[y][x] + 1
+
+        return risk_sum
+
+
+if __name__ == '__main__':
+    Day09().run()
