a0077edffabc16ba98ed6f9a46574b44cda1db5a
Daniel Pettersson <daniel.g.pettersson@gmail.com>
2021-12-09 20:31:58 +0100

    Day 9 complete

--- src/day09_part2.py
+++ src/day09_part2.py
@@ -1,38 +1,86 @@
-from collections import deque
-from typing import TextIO, Any, Optional
+from typing import TextIO, Any, List, Tuple, Callable, Set
+
+from colorama import Fore
 
 from src.common.base_day import BaseDay
 
 
 class Day09(BaseDay):
+    height_map: List[List[int]]
+    width: int
+    height: int
+    low_points: List[Tuple[int, int]]
+    basins: List[List[Tuple[int, int]]]
 
     def execute(self, input_file: TextIO) -> Any:
-        height_map = [[int(v) for v in line.strip()] for line in input_file]
+        self.height_map = [[int(v) for v in line.strip()] for line in input_file]
+        self.width = len(self.height_map[0])
+        self.height = len(self.height_map)
+        self.low_points = []
+        self.basins = []
 
-        width = len(height_map[0])
-        height = len(height_map)
-
-        low_points = []
-
-        for y in range(0, height):
-            for x in range(0, width):
-                larger_neighbors = 0
-                for dx, dy in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
-                    xx = x + dx
-                    yy = y + dy
-                    if 0 <= xx < width and 0 <= yy < height:
-                        larger_neighbors += 1 if height_map[y][x] < height_map[yy][xx] else 0
-                    else:
-                        larger_neighbors += 1
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                larger_neighbors = sum(self._visit_neighbors(
+                    x, y,
+                    lambda xx, yy: 1 if self.height_map[y][x] < self.height_map[yy][xx] else 0,
+                    lambda: 1
+                ))
                 if larger_neighbors == 4:
-                    low_points.append((x, y))
+                    self.low_points.append((x, y))
 
-        basins = []
+        for low_point in self.low_points:
+            basin_coords = []
+            self._find_basin(basin_coords, low_point[0], low_point[1])
+            self.basins.append(basin_coords)
 
-        for low_point in low_points:
+        sorted_basins = sorted(self.basins, key=lambda b: len(b), reverse=True)
+        self._print_output()
 
+        return len(sorted_basins[0]) * len(sorted_basins[1]) * len(sorted_basins[2])
 
-        return low_points
+    def _print_output(self):
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                coord = (x, y)
+                if coord in self.low_points:
+                    print(f"{Fore.RED}{self.height_map[y][x]}{Fore.RESET}", end="")
+                elif self._is_coord_in_basin(coord):
+                    print(f"{Fore.BLUE}{self.height_map[y][x]}{Fore.RESET}", end="")
+                else:
+                    print(self.height_map[y][x], end="")
+            print("")
+
+    def _is_coord_in_basin(self, coord: Tuple[int, int]) -> bool:
+        for basin_coords in self.basins:
+            if coord in basin_coords:
+                return True
+        return False
+
+    def _find_basin(self, basin_coords: List[Tuple[int, int]], x: int, y: int) -> None:
+        def visit_in_range(xx: int, yy: int) -> None:
+            if self.height_map[yy][xx] < 9 and (xx, yy) not in basin_coords:
+                basin_coords.append((xx, yy))
+                self._find_basin(basin_coords, xx, yy)
+
+        self._visit_neighbors(
+            x, y,
+            visit_in_range,
+            lambda: None
+        )
+
+    def _visit_neighbors(self, x: int, y: int,
+                         visit_in_range: Callable[[int, int], Any],
+                         visit_out_of_range: Callable[[], Any]):
+        res = []
+        for dx, dy in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
+            xx = x + dx
+            yy = y + dy
+            if 0 <= xx < self.width and 0 <= yy < self.height:
+                res.append(visit_in_range(xx, yy))
+            else:
+                res.append(visit_out_of_range())
+        return res
 
 
 if __name__ == '__main__':
